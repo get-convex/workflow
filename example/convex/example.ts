9735d8ffb05dd9707cdb10396a402d7cb8b46b3e
Ian Macartney <ianhmacartney@gmail.com>
2025-07-09 19:45:07 -0700

    add startAsync to example

--- example/convex/example.ts
+++ example/convex/example.ts
@@ -74,6 +74,7 @@ export const startWorkflow = internalMutation({
       {
         onComplete: internal.example.flowCompleted,
         context: { location },
+        startAsync: true,
       },
     );
     await ctx.db.insert("flows", { workflowId: id, in: location, out: null });

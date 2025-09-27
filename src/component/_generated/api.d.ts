fa9fffd064274db9507aa67f84fabb41df305804
Ian Macartney <ianhmacartney@gmail.com>
2025-09-26 17:13:51 -0700

    update workpool fix

--- src/component/_generated/api.d.ts
+++ src/component/_generated/api.d.ts
@@ -263,7 +263,6 @@ export declare const components: {
         "internal",
         {
           before?: number;
-          limit?: number;
           logLevel: "DEBUG" | "TRACE" | "INFO" | "REPORT" | "WARN" | "ERROR";
         },
         any

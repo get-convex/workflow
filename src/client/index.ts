86ee8f5bba74a150e3e6fb056b0d624266698a54
fern <60588558+ferntheplant@users.noreply.github.com>
2025-10-09 17:18:18 -0400

    Update index.ts

--- src/client/index.ts
+++ src/client/index.ts
@@ -20,8 +20,8 @@ import { safeFunctionName } from "./safeFunctionName.js";
 import type { OpaqueIds, WorkflowComponent, WorkflowStep } from "./types.js";
 import { workflowMutation } from "./workflowMutation.js";
 
-export { vWorkflowId, type WorkflowId, type WorkflowStep } from "../types.js";
-export type { RunOptions } from "./types.js";
+export { vWorkflowId, type WorkflowId } from "../types.js";
+export type { RunOptions, WorkflowStep } from "./types.js";
 
 export type CallbackOptions = {
   /**

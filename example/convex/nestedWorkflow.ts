4a34800404ba0cfe5ae9be7ae9a92df18e237c4e
Ian Macartney <ianhmacartney@gmail.com>
2025-10-17 14:07:13 -0700

    add step to example

--- example/convex/nestedWorkflow.ts
+++ example/convex/nestedWorkflow.ts
@@ -1,25 +1,37 @@
 import { v } from "convex/values";
 import { workflow } from "./example";
 import { internal } from "./_generated/api";
+import { internalMutation } from "./_generated/server";
 
 export const parentWorkflow = workflow.define({
   args: { prompt: v.string() },
-  returns: v.null(),
   handler: async (ctx, args) => {
     console.log("Starting confirmation workflow");
     const length = await ctx.runWorkflow(
-      internal.nestedWorkflow.nestedWorkflow,
+      internal.nestedWorkflow.childWorkflow,
       { foo: args.prompt },
     );
     console.log("Length:", length);
+    const stepResult = await ctx.runMutation(internal.nestedWorkflow.step, {
+      foo: args.prompt,
+    });
+    console.log("Step result:", stepResult);
   },
 });
 
-export const nestedWorkflow = workflow.define({
+export const childWorkflow = workflow.define({
   args: { foo: v.string() },
   returns: v.number(),
-  handler: async (_, args) => {
+  handler: async (_ctx, args) => {
     console.log("Starting nested workflow");
+    return args.foo.length;
+  },
+});
+
+export const step = internalMutation({
+  args: { foo: v.string() },
+  handler: async (_ctx, args) => {
+    console.log("Starting step");
     return args.foo.length;
   },
 });

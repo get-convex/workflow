a5ab34f05c1e44b13b8285e9be5ed40265718567
Ian Macartney <ianhmacartney@gmail.com>
2025-11-04 00:50:41 -0800

    component api

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,5 @@
 import type { TestConvex } from "convex-test";
 import type { GenericSchema, SchemaDefinition } from "convex/server";
-import workpool from "@convex-dev/workpool/test";
 import schema from "./component/schema.js";
 const modules = import.meta.glob("./component/**/*.ts");
 
@@ -9,11 +8,10 @@ const modules = import.meta.glob("./component/**/*.ts");
  * @param t - The test convex instance, e.g. from calling `convexTest`.
  * @param name - The name of the component, as registered in convex.config.ts.
  */
-function register(
+export function register(
   t: TestConvex<SchemaDefinition<GenericSchema, boolean>>,
-  name: string = "workflow",
+  name: string = "workflow"
 ) {
   t.registerComponent(name, schema, modules);
-  workpool.register(t, `${name}/workpool`);
 }
 export default { register, schema, modules };

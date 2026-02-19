28c53f9db0a044d26325141b1c3c359f900d87af
Ian Macartney <366683+ianmacartney@users.noreply.github.com>
2026-02-18 21:17:49 -0800

    turn getting entry into warning when missing and refactor getting workflow when not checking generation number

--- src/component/model.ts
+++ src/component/model.ts
@@ -1,37 +1,19 @@
+import type { Id } from "./_generated/dataModel.js";
 import type { QueryCtx } from "./_generated/server.js";
 
 export async function getWorkflow(
   ctx: QueryCtx,
-  workflowIdStr: string,
-  expectedGenerationNumber: number | null,
+  workflowId: Id<"workflows">,
+  expectedGenerationNumber: number,
 ) {
-  const workflowId = ctx.db.normalizeId("workflows", workflowIdStr);
-  if (!workflowId) {
-    throw new Error(`Invalid workflow ID: ${workflowIdStr}`);
-  }
-  const workflow = await ctx.db.get(workflowId);
+  const workflow = await ctx.db.get("workflows", workflowId);
   if (!workflow) {
     throw new Error(`Workflow not found: ${workflowId}`);
   }
-  if (
-    expectedGenerationNumber !== null &&
-    workflow.generationNumber !== expectedGenerationNumber
-  ) {
+  if (workflow.generationNumber !== expectedGenerationNumber) {
     throw new Error(
       `Invalid generation number: ${expectedGenerationNumber} for workflow ${workflow.name} (${workflowId})`,
     );
   }
   return workflow;
-}
-
-export async function getJournalEntry(ctx: QueryCtx, journalIdStr: string) {
-  const journalId = ctx.db.normalizeId("steps", journalIdStr);
-  if (!journalId) {
-    throw new Error(`Invalid journal ID: ${journalIdStr}`);
-  }
-  const journalEntry = await ctx.db.get(journalId);
-  if (!journalEntry) {
-    throw new Error(`Journal entry not found: ${journalId}`);
-  }
-  return journalEntry;
 }

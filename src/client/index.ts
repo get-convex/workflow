8763d027c8da2e69981eb0f18c01add7df58fd26
Ian Macartney <366683+ianmacartney@users.noreply.github.com>
2026-04-10 01:19:32 -0700

    prefer workflow.define().handler()

--- src/client/index.ts
+++ src/client/index.ts
@@ -486,7 +486,19 @@ export class WorkflowManager {
   ) {}
 
   /**
-   * Define a new workflow.
+   * Define a new workflow, specifying the args and returns, then handler.
+   *
+   * This approach is recommended over defining the handler inline to avoid
+   * circular types.
+   *
+   * @example
+   * ```ts
+   * export const doSomething = defineWorkflow({
+   *   args: { amount: v.number() },
+   *   returns: v.object({ total: v.number() }),
+   * }).handler(async (step, args) => {
+   *   ...workflow implementation
+   * });
    *
    * Start the workflow from a mutation or action:
    * ```ts
@@ -505,14 +517,8 @@ export class WorkflowManager {
     ReturnsValidator extends Validator<unknown, "required", string> | void,
   >(
     workflow: WorkflowDefinition<ArgsValidator, ReturnsValidator> & {
-      handler: WorkflowHandler<ArgsValidator, ReturnsValidator>;
+      handler?: never;
     },
-  ): RegisteredMutation<"internal", WorkflowArgs<ArgsValidator>, WorkflowId>;
-  define<
-    ArgsValidator extends PropertyValidators,
-    ReturnsValidator extends Validator<unknown, "required", string> | void,
-  >(
-    workflow: WorkflowDefinition<ArgsValidator, ReturnsValidator>,
   ): {
     /**
      * Define the workflow handler function.
@@ -525,6 +531,20 @@ export class WorkflowManager {
       ) => Promise<ReturnValueForOptionalValidator<ReturnsValidator>>,
     ): RegisteredMutation<"internal", WorkflowArgs<ArgsValidator>, WorkflowId>;
   };
+  /**
+   * Define a new workflow.
+   *
+   * @param workflow - The workflow definition.
+   * @returns The workflow mutation.
+   */
+  define<
+    ArgsValidator extends PropertyValidators,
+    ReturnsValidator extends Validator<unknown, "required", string> | void,
+  >(
+    workflow: WorkflowDefinition<ArgsValidator, ReturnsValidator> & {
+      handler: WorkflowHandler<ArgsValidator, ReturnsValidator>;
+    },
+  ): RegisteredMutation<"internal", WorkflowArgs<ArgsValidator>, WorkflowId>;
   define<
     ArgsValidator extends PropertyValidators,
     ReturnsValidator extends Validator<unknown, "required", string> | void,
